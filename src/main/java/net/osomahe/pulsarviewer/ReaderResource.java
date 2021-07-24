809c4978271b783903dacdc8deed8a744789d14a
Antonin Stoklasek <antonin.stoklasek@gmail.com>
2021-07-24 10:33:58 +0200

    filtering based on message id or json path predicate

--- src/main/java/net/osomahe/pulsarviewer/ReaderResource.java
+++ src/main/java/net/osomahe/pulsarviewer/ReaderResource.java
@@ -1,10 +1,12 @@
 package net.osomahe.pulsarviewer;
 
 import javax.inject.Inject;
+import javax.validation.constraints.NotEmpty;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import java.util.Optional;
 
 @Path("/read")
 public class ReaderResource {
@@ -13,7 +15,18 @@ public class ReaderResource {
     ReaderService service;
 
     @GET
-    public Response readMessages(@QueryParam("topic") String topicName) {
-        return Response.ok(service.readStringMessage(topicName)).build();
+    public Response readMessages(
+            @NotEmpty @QueryParam("topic") String topicName,
+            @QueryParam("messageId") Optional<String> messageId,
+            @QueryParam("jsonPathPredicate") Optional<String> jsonPathPredicate
+    ) {
+        if (messageId.isPresent()) {
+            Optional<ReaderMessage> message = service.readStringMessage(topicName, messageId.get());
+            if (message.isPresent()) {
+                return Response.ok(message).build();
+            }
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(service.readStringMessage(topicName, jsonPathPredicate)).build();
     }
 }
--- src/main/java/net/osomahe/pulsarviewer/ReaderService.java
+++ src/main/java/net/osomahe/pulsarviewer/ReaderService.java
@@ -1,6 +1,9 @@
 package net.osomahe.pulsarviewer;
 
+import com.jayway.jsonpath.InvalidPathException;
+import com.jayway.jsonpath.JsonPath;
 import org.apache.pulsar.client.api.*;
+import org.apache.pulsar.client.internal.DefaultImplementation;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
@@ -10,6 +13,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 @ApplicationScoped
@@ -23,7 +27,7 @@ public class ReaderService {
     @Inject
     PulsarClient pulsarClient;
 
-    public List<ReaderMessage> readStringMessage(String topicName) {
+    public List<ReaderMessage> readStringMessage(String topicName, Optional<String> jsonPathPredicate) {
 
         try (Reader<String> reader = pulsarClient.newReader(Schema.STRING)
                 .readerName(readerName)
@@ -31,19 +35,52 @@ public class ReaderService {
                 .startMessageId(MessageId.earliest)
                 .create()) {
             List<ReaderMessage> messages = new ArrayList<>();
-            while (true) {
+            while (reader.hasMessageAvailable()) {
                 var message = reader.readNext(1, TimeUnit.SECONDS);
                 if (message == null) {
                     break;
                 }
-                messages.add(new ReaderMessage(message));
+                var readerMessage = new ReaderMessage(message);
+                if (jsonPathPredicate.isPresent()) {
+                    try {
+                        List result = JsonPath.parse(readerMessage.value).read(jsonPathPredicate.get());
+                        if (result.size() > 0) {
+                            messages.add(readerMessage);
+                        }
+                    } catch (InvalidPathException e) {
+                        log.debugf("Invalid JsonPath: %s for data: %s", readerMessage.value);
+                    }
+                } else {
+                    messages.add(readerMessage);
+                }
             }
             return messages;
-        } catch (PulsarClientException e) {
-            e.printStackTrace();
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("Cannot create pulsar reader for topic: " + topicName);
         }
         return Collections.emptyList();
+    }
+
+    public Optional<ReaderMessage> readStringMessage(String topicName, String messageId) {
+        try (Reader<String> reader = pulsarClient.newReader(Schema.STRING)
+                .readerName(readerName)
+                .startMessageIdInclusive()
+                .startMessageId(getMessageId(messageId))
+                .topic(topicName)
+                .create()) {
+            Message<String> message = reader.readNext();
+            if (message != null) {
+                return Optional.of(new ReaderMessage(message));
+            }
+        } catch (IOException e) {
+            log.error("Cannot create pulsar reader for topic: " + topicName);
+        }
+        return Optional.empty();
+    }
+
+    private MessageId getMessageId(String messageId) {
+        String[] parts = messageId.split(":");
+        return DefaultImplementation.newMessageId(
+                Long.parseLong(parts[0]), Long.parseLong(parts[1]), Integer.parseInt(parts[2]));
     }
 }
